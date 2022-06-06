62dd52c1708fb1799c5b26f044469fa14aac9597
mattowsh <5089@holbertonschool.com>
2022-06-06 09:05:52 -0700

    versioin 1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sentence and return PID = errors quantity
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
