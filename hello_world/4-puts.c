6009fd5eb3d1954d494af2bb2cf11642903ac8c2
mattowsh <5089@holbertonschool.com>
2022-06-06 07:53:09 -0700

    comment fixed by betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* 
- * main: is the main function in this C script
+/**
+ * main: is the main function in this C script, prints "Programming is like building a multilingual puzzle" and return PID = errors quantity
  */
 
 int main(void)
