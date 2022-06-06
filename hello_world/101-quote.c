1ab8969b4d323728bf40a584d13a7ba4d5c1611a
mattowsh <5089@holbertonschool.com>
2022-06-06 10:46:38 -0700

    version 1

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - iiiiiiiiiiiiiii
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"; 59); 
+	/* 2: for stderr*; 59: for the number of characters(bytes) in the sentences */
+
+	return (1);
+}
