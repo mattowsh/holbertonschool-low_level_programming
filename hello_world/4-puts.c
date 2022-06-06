f05be18b68f37d83553478b2e89007405b30f7d5
mattowsh <5089@holbertonschool.com>
2022-06-06 07:58:20 -0700

    betty style version 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main: is the main function in this C script, prints "Programming is like building a multilingual puzzle" and return PID = errors quantity
+ * main: prints Programming is like building a multilingual puzzle and return PID = errors quantity
  */
 
 int main(void)
@@ -9,3 +9,7 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
+/**
+ * if don't have any error, main prints the sentence and return 0
+ */
