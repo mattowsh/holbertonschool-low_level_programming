e1c2c53ba95e31329ebfd3049a5113e30263b70e
mattowsh <5089@holbertonschool.com>
2022-06-09 06:47:08 -0700

    version 0

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * _abs - Prints the absolute value of an integer
+ * @n: integer
+ * Return: Always 0 (Sucess)
+ */
+
+int _abs(int n)
+{
+	int num = abs(n);
+
+	return (num);
+}
