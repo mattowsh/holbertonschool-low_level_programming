a1b475d0c602587394ff9d7757b0f4a68e231e61
mattowsh <5089@holbertonschool.com>
2022-06-11 08:33:31 -0700

    betty style ok

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,11 +3,11 @@
 
 /**
  * _isupper - Checks for uppercase character
- *
+ * @c : character
  * Return: 1 if letter si uppercase, 0 otherwise
  */
 
-int _isupper (int c)
+int _isupper(int c)
 {
 	if (isupper(c)) /* command from ctype.h library */
 	{
