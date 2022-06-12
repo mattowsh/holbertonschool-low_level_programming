669da907b79a82087c38b1656d9e71bccc2bf0dd
mattowsh <5089@holbertonschool.com>
2022-06-11 17:45:33 -0700

    version 0

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100:
+ * multiples of 3: prints Fizz
+ * multiples of 5: prints Buzz
+ * multiples of 3 & 5: prints FizzBuzz
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
