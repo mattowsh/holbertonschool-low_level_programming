72a8da012448f207a1787e2f68945347b4855a78
mattowsh <5089@holbertonschool.com>
2022-06-15 12:17:38 -0700

    version 0

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while(i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+		if (i < n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
