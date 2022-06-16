270630ab55eb9676bba4c89e2b228588d7870753
mattowsh <5089@holbertonschool.com>
2022-06-16 10:51:49 -0700

    version 0

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+
+	/* to check if the variables have available memory */
+	if (!dest)
+		return (0);
+	if (!src)
+		return (0);
+
+
+	while (dest[i]) /* calculates the length of the dest */
+		i++;
+	
+	if (dest[i] == '\0' || src[j] == '\0')
+		return (dest);
+	else if (n > 0)
+	{
+		for (; j < n; j++, i++)
+			dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
