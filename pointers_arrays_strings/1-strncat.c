82a4802c5f18150f42812cdab8dd4b5ae843536a
mattowsh <5089@holbertonschool.com>
2022-06-16 10:56:00 -0700

    version 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -18,19 +18,19 @@ char *_strncat(char *dest, char *src, int n)
 	if (!src)
 		return (0);
 
-
-	while (dest[i]) /* calculates the length of the dest */
-		i++;
 	
 	if (dest[i] == '\0' || src[j] == '\0')
 		return (dest);
 	else if (n > 0)
 	{
+		while (dest[i]) /* calculates the length of the dest */
+			i++;		
 		for (; j < n; j++, i++)
 			dest[i] = src[j];
+		
+		dest[i] = '\0';
 	}
 
-	dest[i] = '\0';
 
 	return (dest);
 }
