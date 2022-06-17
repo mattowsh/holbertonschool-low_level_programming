011673b985ce8365776ab2da61778a5b2cdd7028
mattowsh <5089@holbertonschool.com>
2022-06-16 18:31:01 -0700

    version 3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
+ * _strncpy - Concatenates two strings
+ * @src : source of my data
+ * @dest : final destiny where I will print my data
+ * @n : quantity of chars to be printed
  *
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
-	i = j = 0;
 
-	/* to check if the variables have available memory */
-	if (!dest)
-		return (0);
-	if (!src)
-		return (0);
+	/* border case */
+	if (n == 0)
+		return (dest);
 
-	/* calculates the length of source */
+	/* calculates the length of source, without \0 considered */
+	/* if str = "" --> i = 0 */
 	for (i = 0; src[i]; i++)
 		;
 
-	if (n == 0 || src[0] == "")
-		return (dest);
-	
-	if (i < n)
-		n = i;
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+	/* cop the n chars */
+	for (j = 0; j < n; j++)
+	{
+		if (j <= i)
+			dest[j] = src[j];
+		else /* (j > i) */
+			dest[j] = '\0';
+	}
 
 	return (dest);
 }
-
