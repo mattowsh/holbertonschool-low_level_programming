60b656bbd5cf1531b8cb92548ed6aeb36f7a3d53
mattowsh <5089@holbertonschool.com>
2022-06-19 16:17:23 -0700

    NULL return

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - Locates a character in a string
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 			return (s);
 	}
 
-	return ('\0');
+	return (NULL);
 }
