67fcd37d1a8c0e1c1714724aefd1f66375cd5a11
mattowsh <5089@holbertonschool.com>
2022-06-19 16:27:50 -0700

    case: c == void

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,16 +12,15 @@
 
 char *_strchr(char *s, char c)
 {
-	/* case: char to locate: void */
-	if (!s[0])
-		return (s);
-
-
 	for (; s[0]; s++) /* s++: move the pointer */
 	{
 		if (s[0] == c) /* s[0] is always the value that the pointer marks */
 			return (s);
 	}
+
+	/* case: char to locate: void: the pointer is on the \0 of my string */
+	if (c == '\0')
+		return (s);
 
 	return (NULL);
 }
