d329095a8ac6a89343afefd24b2466df71596362
mattowsh <5089@holbertonschool.com>
2022-06-21 10:15:33 -0700

    version 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mynameis - Prints its name, followed by a new line
+ * @argc : argument count
+ * @argv : argument variable
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
