be1a1effee1d21d7b29a99bd813b61f8b0308868
mattowsh <5089@holbertonschool.com>
2022-06-23 10:10:52 -0700

    version 0

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Concatenates two strings
+ * @s1 : string destination
+ * @s2 : string to move
+ *
+ * Return : NULL if size = 0, ar otherwise
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int length, i, j;
+	char *ar;
+
+	/* exepciones */
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	/*calculate length of s1 + s2*/
+	for (length = 0; s1[length]; length++)
+		;
+	for (; s2[length]; length++)
+		;
+
+	ar = malloc(length + 1 * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		ar[i] = s1[i];
+
+	for (j = 0; s2[j]; j++, i++)
+		ar[i] = s2[j];
+
+	ar[i] = '\0';
+
+	return (ar);
+}
