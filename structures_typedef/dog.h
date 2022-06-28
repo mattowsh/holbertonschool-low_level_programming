5cf6a56fb611b24724a65a927306469cd1379e1e
mattowsh <5089@holbertonschool.com>
2022-06-28 10:32:16 -0700

    structure name was changed

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+/* functions */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 /**
  * struct dog - dog info
  * @name : dog's name
@@ -9,14 +13,11 @@
  *
  */
 
-struct dog
+struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 #endif
