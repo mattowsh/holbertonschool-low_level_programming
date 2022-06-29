61563511a339311081ed90bdfa61bc40927c5b5e
mattowsh <5089@holbertonschool.com>
2022-06-29 07:42:32 -0700

    dog.h from task 4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,18 +13,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
-
-/**
- * dog_t - dog info
- * @name : dog's name
- * @age : dog's age
- * @owner : owner
- */
-typedef struct dog dog_t;
+} dog_t;
 
 /* functions */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
