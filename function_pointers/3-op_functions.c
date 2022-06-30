14238f74fd1882d2957c7e456b5711fcf0e720c5
mattowsh <5089@holbertonschool.com>
2022-06-30 05:39:38 -0700

    for copy text

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,16 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - sum two integers
+ * @a : number a
+ * @b : number b
+ *
+ * Return: the result of the sum
+ */
+
+int op_add(int a, int b)
+{
+}
+
+
