08f33b4669e944eb7e9d8abb7f2fedc7ed2b05aa
mattowsh <5089@holbertonschool.com>
2022-06-30 19:01:33 -0700

    version 7

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,14 +16,13 @@ int main(int argc, char *argv[])
 	int result = 0;
 	int (*f)(int, int);
 
-	f = get_op_func(argv[2]);
-
-	if (!(argc == 4))
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
+	f = get_op_func(argv[2]);
 	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
