fddae95b53225c9ce7e6e2b4d17a2c74bec5e3af
mattowsh <5089@holbertonschool.com>
2022-07-03 16:01:51 -0700

    .h from task 3 with structure description

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct _print - type of argument and function associated info
+ * @letter : type of argument (format[i])
+ * @f : function associated
+ */
+
+typedef struct _print
+	{
+		char *letter;
+		void (*f)(va_list args, char *separator);
+	} _print_t;
+
+/* functions */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
