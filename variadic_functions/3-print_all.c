e1c96d6d140ff2d0c2db0f1f52593decd879c0c4
mattowsh <5089@holbertonschool.com>
2022-07-04 06:03:40 -0700

    version 4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -27,7 +27,7 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	/* (format != NULL) ? va_start(args, format) : format = '\0'; */
 
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (data[j].letter)
