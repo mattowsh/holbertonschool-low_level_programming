912bc66577e44be4cb8eab8c6a3bfd8680a557f2
mattowsh <5089@holbertonschool.com>
2022-07-14 14:04:04 -0700

    version 0

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head : the first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	if (*head != NULL)
+	{
+		aux = *head;
+		*head = (*head)->next;
+		free(aux);
+	}
+}
