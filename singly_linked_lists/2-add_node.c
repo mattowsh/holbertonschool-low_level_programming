93575b9b47a0ad9add292abfe3c7c2c6990c28b6
mattowsh <5089@holbertonschool.com>
2022-07-13 17:48:58 -0700

    while and strdup fixed

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list
@@ -11,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int i = 0;
+	char *duplicated = strdup(str);
 
 	list_t *newnode = NULL;
 
@@ -19,10 +21,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	/* to calculate the length of the string */
-	while (str != '\0')
+	while (str[i] != '\0')
 		i++;
 
-	newnode->str = strdup(str);
+	newnode->str = duplicated;
 	newnode->len = i;
 	newnode->next = *head;
 	*head = newnode;
