d14827c78d68b42c33c0c01fa13ee289de934d68
mattowsh <5089@holbertonschool.com>
2022-07-14 06:13:22 -0700

    list_t aux was created

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - Add a new node at the end of a list_t list
+ * @head : the first node
+ * @str : string to be added to the new node
+ *
+ * Return: the adress of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *duplicated = strdup(str);
+
+	list_t *newnode = NULL;
+	list_t *aux;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	/* calculated the length of the string */
+	while (str[i] != '\0')
+		i++;
+
+	newnode->str = duplicated;
+	newnode->len = i;
+	newnode->next = NULL;
+
+	/* changes the "next" of the last node to newnode */
+	if (*head == NULL)
+		*head = newnode;
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+			aux = aux->next;
+		aux->next = newnode;
+	}
+
+	return (newnode);
+}
