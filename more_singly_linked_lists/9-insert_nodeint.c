55649c1dc268bbd6efb81209a4b8ec95cce97c06
mattowsh <5089@holbertonschool.com>
2022-07-15 15:25:38 -0700

    while fixed

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
-#include "2-add_nodeint.c"
-#include "3-add_nodeint_end.c"
+
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position
  * @head : the first node
@@ -12,9 +11,9 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0; /* to controle the position of my new node */
+	unsigned int i = 0; /* to control the position of my new node */
 
-	listint_t *currentNode;
+	listint_t *currentNode = *head;
 	listint_t *newnode = NULL;
 
 	newnode = malloc(sizeof(listint_t));
@@ -22,27 +21,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newnode->n = n;
 
-	currentNode = *head;
+
+	if (idx == 0) /* at the beginning */
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+
 	while (currentNode != NULL)
 	{
-		if (idx == 0) /* at the beginning */
-		{
-			newnode = add_nodeint(head, n);
-			return (newnode);
-		}
-
 		if (i == idx - 1) /* at the middle */
 		{
-			currentNode->next = newnode;
 			newnode->next = (currentNode)->next;
+			currentNode->next = newnode;
 			return (newnode);
 		}
-
 		i++;
-		currentNode = (currentNode)->next;
 	}
 
 	/* at the end */
-	newnode = add_nodeint_end(head, n);
+	newnode->next = NULL;
+
 	return (newnode);
 }
