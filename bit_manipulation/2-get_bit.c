0ac4fd7a8e5975b38ab16c8eb72a955223161cc2
mattowsh <5089@holbertonschool.com>
2022-07-19 12:49:34 -0700

    else fixed

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,11 +12,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int binary_value = (n >> index) & 1;
 
-	if (index <= 64)
+	if (index < 64)
 	{
 		if (binary_value == 0 || binary_value == 1)
 			return (binary_value);
 	}
-	else
-		return (-1);
+	
+	return (-1);
 }
