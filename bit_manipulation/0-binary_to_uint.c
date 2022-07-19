63068dd98f184d101876e01074312f73f0644362
mattowsh <5089@holbertonschool.com>
2022-07-19 10:12:45 -0700

    if condition fixed

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -20,18 +20,16 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i] != '\0')
 	{
-		if (b[i] != 0 && b[i] != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		length++;
 		i++;
 	}
 
-
 	for (i = length - 1; i >= 0; i--)
 	{
 		if (b[i] == 1)
 			conversion += n;
-
 		n += n;
 	}
 
