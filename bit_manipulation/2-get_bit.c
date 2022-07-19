e4c631cef49f873d08eea3914ebc2f526652aec8
mattowsh <5089@holbertonschool.com>
2022-07-19 12:42:22 -0700

    if index fixed

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,8 +12,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int binary_value = (n >> index) & 1;
 
-	if (binary_value == 0 || binary_value == 1)
-		return (binary_value);
+	if (index <= 64)
+	{
+		if (binary_value == 0 || binary_value == 1)
+			return (binary_value);
+	}
 	else
 		return (-1);
 }
