1d442133568c662e4fd85a6d76b8e3f5e5651593
mattowsh <5089@holbertonschool.com>
2022-07-21 07:07:49 -0700

    README.md created

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ * @h : the first node
+ *
+ * Return: the number of nodes
+ */
+
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next)
+	{
+		printf("%n\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	printf("%n\n", h->n);
+	return (i + 1);
+}
