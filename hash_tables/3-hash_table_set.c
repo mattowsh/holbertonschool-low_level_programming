883611b4014f039b4f7a6fd0a117088db2a37c2e
mattowsh <5089@holbertonschool.com>
2022-08-21 13:23:07 -0700

    last comment was fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		nodetocheck = nodetocheck->next;
 	}
 
-	/* if any key == MY key */
+	/* if all keys are != MY key */
 	newnode = malloc(sizeof(hash_node_t));
 	if (!newnode)
 		return (0);
