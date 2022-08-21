aeac683d907fd520c81996ab42e43f4a1a0b396b
mattowsh <5089@holbertonschool.com>
2022-08-20 20:09:49 -0700

    .h update from task 4; task 4 file was created; trash files were deleted

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht : the hash table you want to look into
+ * @key : the key you are looking for
+ *
+ * Return: the value associated with the element, or NULL if
+ * key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+}
