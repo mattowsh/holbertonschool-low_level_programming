31a70aff74506ae3ef7e2124a57ce9b2f5e821cf
mattowsh <5089@holbertonschool.com>
2022-08-21 13:52:22 -0700

    task 4 version 0

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,4 +11,24 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	int i = 0, array_length = 0;
+	hash_node_t *nodetocheck =  NULL;
+
+	if (!ht)
+		return (NULL);
+
+	array_length = ht->size;
+
+	for (; i < array_length; i++)
+	{
+		nodetocheck = ht->array[i];
+		while (nodetocheck)
+		{
+			if (strcmp(nodetocheck->key, key) == 0)
+				return (nodetocheck->value);
+			nodetocheck = nodetocheck->next; /*bc array[i]=lilist*/
+		}
+	}
+
+	return (NULL);
 }
