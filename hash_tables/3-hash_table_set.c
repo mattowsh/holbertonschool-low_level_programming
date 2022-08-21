80d07d02deb55fc4d7969ba71d34e2e09866502b
mattowsh <5089@holbertonschool.com>
2022-08-20 19:17:12 -0700

    .h updates: from task 3 & string.h library was addded; task 3 file was created, version 0

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht : the hash table you want to add or update the key/value to
+ * @key : the key, can not be an empty string
+ * @value : the value associated with the key, can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int array_length = 0;
+	unsigned long int keyindex = 0;
+	hash_node_t *nodetocheck = NULL, *newnode = NULL;
+
+	if (!ht)
+		ht = hash_table_create(1);
+
+	array_length = ht->size;
+	keyindex = key_index((const unsigned char *)key, array_length);
+
+	nodetocheck = ht->array[keyindex];
+
+	/* check if in ht->array[keyindex] MY key already exists */
+	while (nodetocheck)
+	{
+		if (strcmp(nodetocheck->key, key) == 0)
+		{
+			free(nodetocheck->value);
+			nodetocheck->value = strdup(value);
+			return (1);
+		}
+		nodetocheck = nodetocheck->next;
+	}
+
+	/* if any key == MY key */
+	newnode = malloc(sizeof(hash_node_t));
+	if (!newnode)
+		return (0);
+
+	newnode->key = strdup(key); /* malloc was considered in strdup */
+	newnode->value = strdup(value);
+	newnode->next = ht->array[keyindex];
+	ht->array[keyindex] = newnode;
+
+	return (1);
+}
