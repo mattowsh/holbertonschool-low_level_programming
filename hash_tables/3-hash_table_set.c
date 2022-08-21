9a1d0d4757b9330457fbe59b775da8dc8da4a744
mattowsh <5089@holbertonschool.com>
2022-08-20 20:05:34 -0700

    !ht fixed: must return 0

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *nodetocheck = NULL, *newnode = NULL;
 
 	if (!ht)
-		ht = hash_table_create(1);
+		return (0);
 
 	array_length = ht->size;
 	keyindex = key_index((const unsigned char *)key, array_length);
